f759e63e1c6df5996f35b6143e26897edc6a482d
gabrielzinhomitinho <gabriel.carmo.alencar@escola.pr.gov.br>
2025-08-05 09:41:37 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ function setup() {
 }
 
 function draw() {
-  background(205, 102, 94); //cor de fundo
+  background(255, 155, 38); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
+  sphere(110, 160); //esfera 3D
   
   
 }
